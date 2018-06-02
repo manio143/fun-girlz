283d7fa1507e44c1ba688157fbb30ee205d1f547
Marian Dziubiak <marian.dziubiak@gmail.com>
2018-06-02 09:36:26 +0200

    Fix

--- docsrc/content/state_monad.fsx
+++ docsrc/content/state_monad.fsx
@@ -16,12 +16,12 @@ type State<'TState, 'TResult> = State of ('TState -> 'TResult * 'TState)
 (*** hide ***)
 type Random = Random of int
 let nextRandom (Random s) = let s' = (s * 123456 + 81327) % System.Int32.MaxValue in (s', Random s)
-let stateReturn x = State <| fun s -> s,x
+let stateReturn x = State <| fun s -> x, s
 let stateRun (State f) s = f s
 let stateBind m f = State <| fun s -> let (x, s') = stateRun m s in stateRun (f x) s'
-let stateGet = State <| fun s -> s,s
-let statePut s = State <| fun _ -> s,()
-let stateModify f = stateBind stateGet (fun s -> statePut (f s))
+let stateGet() : State<'t,'t> = State <| fun (s: 't) -> (s,s)
+let statePut (s : 't) : State<'t, unit> = State <| fun _ -> ((),s)
+let stateModify (f :'t -> 't) = stateBind (stateGet()) (statePut << f)
 type StateBuilder() =
     member this.Return(x) = stateReturn x
     member this.Bind(m, f) = stateBind m f
@@ -39,7 +39,7 @@ Napisz funkcje:
 - `stateReturn : 'a -> State<'t, 'a>`, która nie zmieniając stanu zwraca podaną wartość
 - `stateRun    : State<'t, 'a> -> 't -> 'a * 't`, która przyjmuje monadę stanu i pewien stan początkowy i zwraca parę (wynik, nowy stan)
 - `stateBind   : State<'t, 'a> -> ('a -> State<'t, 'b>) -> State<'t, 'b>`
-- `stateGet    : State<'t, 't>` to jest w zasadzie wartość monadyczna, która w wyniu ma aktualny stan
+- `stateGet    : unit -> State<'t, 't>` to jest w zasadzie wartość monadyczna, która w wyniu ma aktualny stan
 - `statePut    : 't -> State<'t, unit>`, która ustawia stan na podany
 - `stateModify : ('t -> 't) -> State<'t, unit>`, która modyfikuje stan podaną funkcją
 
@@ -52,13 +52,13 @@ type RandomState<'a> = State<Random, 'a>
 
 let nextRandomM = 
     state {
-        let! r = stateGet
+        let! r = stateGet()
         let (x, r') = nextRandom r
         do! statePut r'
         return x
     }
 
-let randomArrayM n = 
+let rec randomArrayM n = 
     state {
         if n <= 0 then return []
         else
